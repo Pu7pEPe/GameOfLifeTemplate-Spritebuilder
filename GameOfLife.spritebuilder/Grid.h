979e9d8d1b47bc0f5b667efb6f537ffb37df46d5
Pu7pEPe <home@Homes-iMac.local>
2014-11-01 22:44:58 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -10,11 +10,11 @@
 
 @interface Grid : CCSprite
 
--(void)evolveStep;
--(void)countNeighbors;
-
-
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
+
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
 
 @end
